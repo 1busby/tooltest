7ed2db69e6f7089c8bfb47e77e5aefbeeb3c2e2a
Michael Busby <michaelb@clearlyinnovative.com>
2017-09-07 21:00:15 -0400

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MaterializeModule,
     BrowserModule
   ],
   providers: [],
